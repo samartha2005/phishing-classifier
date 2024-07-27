65ebe54520165193b92406480f778056856cc473
samartha2005 <gaikwadmaruti974@gmail.com>
2024-07-27 13:08:03 +0530

    logging

--- src/logging.py
+++ src/logging.py
@@ -13,5 +13,5 @@ LOGS_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 logging.basicConfig(
     filename= LOGS_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s- %(levelname) - %(message)s",
-    level = logging.INFO,
+    level = logging.INFO
 )
